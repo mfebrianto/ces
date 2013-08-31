369d8d5586957ea7c50c2853c3251dd9a80eb6ff
mfebrianto <michaelfebrianto@gmail.com>
2013-08-31 12:17:39 +1000

    initial commit

---
+++ src/main/java/edu/unsw/cse/comp9323/group1/App.java
@@ -0,0 +1,13 @@
+package edu.unsw.cse.comp9323.group1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
