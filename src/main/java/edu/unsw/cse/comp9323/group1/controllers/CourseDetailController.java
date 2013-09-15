3448ed4aabb53a56a3d750704298c39a0c3cd71a
mfebrianto <michaelfebrianto@gmail.com>
2013-09-15 18:07:51 +1000

    update student home
    Signed-off-by: mfebrianto <michaelfebrianto@gmail.com>

--- src/main/java/edu/unsw/cse/comp9323/group1/controllers/CourseDetailController.java
+++ src/main/java/edu/unsw/cse/comp9323/group1/controllers/CourseDetailController.java
@@ -4,9 +4,9 @@ import java.io.UnsupportedEncodingException;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import edu.unsw.cse.comp9323.group1.DAOs.CourseDAO;
 import edu.unsw.cse.comp9323.group1.models.Course;
@@ -15,12 +15,12 @@ import edu.unsw.cse.comp9323.group1.models.Course;
 @RequestMapping("/courseDetail")
 public class CourseDetailController {
 	
-	@RequestMapping(value = "/{courseID}", method = RequestMethod.GET)
-	public String getCourseDetail(@PathVariable String courseID, ModelMap model) throws UnsupportedEncodingException {
+	@RequestMapping(method = RequestMethod.GET)
+	public String getCourseDetail(@RequestParam("courseId") String courseId, ModelMap model) throws UnsupportedEncodingException {
 		
 		CourseDAO crsDAO = new CourseDAO();
 		Course course = new Course();
-		course = crsDAO.getCourseByID(courseID);
+		course = crsDAO.getCourseByID(courseId);
 		model.addAttribute("course", course);
 		return "courseDetail";
 	
